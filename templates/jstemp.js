8babb50d1f3e84573e9cd844f7fe376f2aaa59e9
BuildTools <jjagodzinski0@gmail.com>
2019-11-07 20:48:47 +0100

    Add a constructor to the template component

--- templates/jstemp.js
+++ templates/jstemp.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './MyComponent.css';
 
 class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
         return (
             <h1>{/* Replace me! */}</h1>
